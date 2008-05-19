ad33c16e1490dd5bb2ca51233f57c0a84a4b00bf
bpfurtado <bpfurtado@5bca19d3-b14a-0410-9d2d-4f5c93e8c730>
2008-05-19 00:40:57 +0000

    First code to create a static version of a LiveJournal blog (Alpha version).

--- src/net/bpfurtado/ljcolligo/LJColligo.java
+++ src/net/bpfurtado/ljcolligo/LJColligo.java
@@ -1,22 +1,22 @@
 /**
  * Created by Bruno Patini Furtado [http://bpfurtado.livejournal.com]
  * Created on 19/11/2007 20:32:09
- * 
+ *
  * This file is part of LJColligo.
- * 
+ *
  * LJColligo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * LJColligo is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with LJColligo.  If not, see <http://www.gnu.org/licenses/>.
- * 
+ *
  * Project page: http://sourceforge.net/projects/ljcolligo/
  */
 
@@ -25,6 +25,7 @@ package net.bpfurtado.ljcolligo;
 import java.io.File;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import net.bpfurtado.ljcolligo.model.Comment;
@@ -37,76 +38,76 @@ import org.apache.log4j.Logger;
 
 /**
  * Main Facade
- * 
+ *
  * @author Bruno Patini Furtado
  */
 public class LJColligo extends LJColligoObservable
 {
-	private static final Logger logger = Logger.getLogger(LJColligo.class);
-	
-	public LJColligo()
-	{
-		super();
-	}
+    private static final Logger logger = Logger.getLogger(LJColligo.class);
 
-	public LJColligo(LJColligoObservable other)
-	{
-		super(other);
-	}
+    public LJColligo()
+    {
+        super();
+    }
 
-	public File downloadAndPersist(String userName, String password, String outputPath)
-	{
-		logger.debug("Start");
-		sendMessageToListeners("Start");
+    public LJColligo(LJColligoObservable other)
+    {
+        super(other);
+    }
 
-		Collection<Event> events = downloadEvents(userName, password);
+    public File downloadAndPersist(String userName, String password, String outputPath)
+    {
+        logger.debug("Start");
+        sendMessageToListeners("Start");
 
-		if (!outputPath.endsWith(File.separator)) {
-			outputPath += File.separator;
-		}
+        Collection<Event> events = downloadEvents(userName, password);
 
-		File outputFile = Util.genFile(userName, outputPath, ".xml");
+        if (!outputPath.endsWith(File.separator)) {
+            outputPath += File.separator;
+        }
 
-		Events2XMLWriter events2XMLWriter = new Events2XMLWriter();
-		events2XMLWriter.addAllListenersFrom(this);
-		events2XMLWriter.write(events, outputFile);
+        File outputFile = Util.genFile(userName, outputPath, ".xml");
 
-		logger.debug("End");
-		sendMessageToListeners("End");
-		
-		return outputFile;
-	}
+        Events2XMLWriter events2XMLWriter = new Events2XMLWriter();
+        events2XMLWriter.addAllListenersFrom(this);
+        events2XMLWriter.write(events, outputFile);
 
-	public Collection<Event> downloadEvents(String userName, String password)
-	{
-		GetEvents getEvents = new GetEvents(this);
-		
-		Collection<Event> events = getEvents.download(userName, password);
+        logger.debug("End");
+        sendMessageToListeners("End");
 
-		GetComments getComments = new GetComments(this);
-		Collection<Comment> comments = getComments.download(userName, password);
+        return outputFile;
+    }
 
-		merge(events, comments);
+    public List<Event> downloadEvents(String userName, String password)
+    {
+        GetEvents getEvents = new GetEvents(this);
 
-		return events;
-	}
+        List<Event> events = getEvents.download(userName, password);
 
-	private void merge(Collection<Event> events, Collection<Comment> comments)
-	{
-		Map<Integer, Event> m = new HashMap<Integer, Event>();
-		for (Event e : events) {
-			m.put(e.getId(), e);
-		}
+        GetComments getComments = new GetComments(this);
+        Collection<Comment> comments = getComments.download(userName, password);
 
-		for (Comment c : comments) {
-			Event event = m.get(c.getEventId());
-			if (event != null) {
-				event.add(c);
-			} else {
-				String msg = "Event with id [" + c.getEventId() + "] not found, comment=" + c;
-				logger.warn(msg);
-				sendMessageToListeners(msg);
-			}
-		}
-	}
+        merge(events, comments);
+
+        return events;
+    }
+
+    private void merge(Collection<Event> events, Collection<Comment> comments)
+    {
+        Map<Integer, Event> m = new HashMap<Integer, Event>();
+        for (Event e : events) {
+            m.put(e.getId(), e);
+        }
+
+        for (Comment c : comments) {
+            Event event = m.get(c.getEventId());
+            if (event != null) {
+                event.add(c);
+            } else {
+                String msg = "Event with id [" + c.getEventId() + "] not found, comment=" + c;
+                logger.warn(msg);
+                sendMessageToListeners(msg);
+            }
+        }
+    }
 }
